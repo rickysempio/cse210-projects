8aea938a73caf2083d873891bc7b5b878464ce18
rickysempio <rickysempio@gmail.com>
2024-02-29 18:04:31 +0800

    change variable

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name? ");
         string name = Console.ReadLine();
         Console.Write("what is your last name? ");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
